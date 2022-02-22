cea6b6bef0f797c9993552ea4d10f4946330b4c6
alexander <alexanderjhon997@gmail.com>
2022-02-22 12:38:28 -0500

    upload all project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="container mt-5">
+             <h1>Tareas</h1>
+              <hr/>
+            <div className="row">
+                 <div className="col -8">
+                                   <h4 className="text-center"> lista de tareas</h4>
+                 </div>
+                 <div className="col-4">
+                                   <h4 className="text-center">lista de tareas</h4>
+                  </div>
+             </div>
+    </div>
+  )
+}
+
+export default App
