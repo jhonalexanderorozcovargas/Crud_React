04429d86073dac53793c18288bcd29a40db24f3a
jhonalexanderorozcovargas <57021735+jhonalexanderorozcovargas@users.noreply.github.com>
2022-03-16 22:09:59 -0500

    se monta frontend basico ala pagina

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-function App() {
-  const [state,setstate] = useState(initialState)
-  return (
-    <div className="container mt-5">
-         <h1>Tareas</h1>
-             <hr/>
-              <div className="row">
-                 <div className="col -8">
-                     <h4 className="text-center"> lista de tareas</h4>
-                        <ul className="list-grup">
-                           <li className="list-grup-item">
-                               <span className="lead">nombre de la tarea</span>
-                               <button className="btn btn-danger btn-sn float right mx-2">Eliminar</button>
-                               <button className="btn btn-warning btn-sn float right mx-2">Editar</button> 
-                            </li>
-                         </ul>
-                 </div>
-                 <div className="col-4">
-                   <h4 className="text-center">formularios</h4>
-                   <form>
-                     <input
-                      type= "text"
-                      className="form-control mb-2"
-                      placeholder="ingrese la tarea.."
-                      />
-                      <button className="btn btn-dark btn-block" type="submit">agregar</button> 
-                   </form>
-                </div>
-             </div>
-    </div>
+function  App() {
+return (
+ <div className="container mt-5"> 
+    <h1>TAREAS</h1>
+     <hr/>
+      <div className="row"> 
+       <div className="col-8">
+          <h4 className="text-center">lista de tareas</h4>
+          <ul className="list-group">
+              <li className="list-group-item">
+                 <span className="lead">nombre de la tarea</span>
+                 <button className="btn btn-danger btn-sm float-right mx-2">eliminar</button>
+                 <button className="btn btn-warning btn-sm float-right ">eliminar</button>
+              </li>
+         </ul>
+       </div>
+      <div className="col-4">
+          <h4 className="text-center">formularios</h4>
+          <form>
+             <input 
+             type="text"
+             className="form-control mb-2"
+             placeholder="ingerse la tarea..."  
+             >
+             </input>
+             <button
+             className="btn btn-dark btn-block"
+             type="submit"
+             >
+             agregar
+             </button>
+          </form>
+       </div>
+     </div>
+   </div>
   )
 }
 
-export default App
+export default App ;
