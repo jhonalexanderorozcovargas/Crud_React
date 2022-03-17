038f67e2298db475348374b1dbdfe8add176a91e
jhonalexanderorozcovargas <57021735+jhonalexanderorozcovargas@users.noreply.github.com>
2022-03-16 22:51:30 -0500

    adicionando nuevas tareas y validando campos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import { isEmpty } from 'lodash'
+import shortid from 'shortid'
+
 
 function  App() {
+   const [task, setTask] = useState("")
+   const [tasks, setTasks] = useState([])
+
+   const addTask = (e) =>{
+      e.preventDefault()
+      if (isEmpty(task)){
+       console.log("task empty")
+       return
+      }
+
+      const newTask =   {
+         id: shortid.generate(),
+         name: task
+      }
+
+      setTasks([...tasks, newTask ])
+
+     setTask("")
+   }
+
+
+
 return (
  <div className="container mt-5"> 
     <h1>TAREAS</h1>
@@ -9,20 +34,26 @@ return (
        <div className="col-8">
           <h4 className="text-center">lista de tareas</h4>
           <ul className="list-group">
-              <li className="list-group-item">
-                 <span className="lead">nombre de la tarea</span>
-                 <button className="btn btn-danger btn-sm float-right mx-2">eliminar</button>
-                 <button className="btn btn-warning btn-sm float-right ">eliminar</button>
-              </li>
+            {
+               tasks.map((task) => (
+                <li className="list-group-item" key={task.id}>
+                   <span className="lead">{task.name}</span>
+                   <button className="btn btn-danger btn-sm float-right mx-2">eliminar</button>
+                   <button className="btn btn-warning btn-sm float-right ">eliminar</button>
+                </li>
+                ))
+            }
          </ul>
        </div>
       <div className="col-4">
           <h4 className="text-center">formularios</h4>
-          <form>
+          <form onSubmit={addTask}>
              <input 
              type="text"
              className="form-control mb-2"
              placeholder="ingerse la tarea..."  
+             onChange={(text) => setTask(text.target.value)}
+             value={task}
              >
              </input>
              <button
